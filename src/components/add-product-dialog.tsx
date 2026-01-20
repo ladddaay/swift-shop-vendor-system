1f139ae2d1b9daed11edd623b0eef73e6aca0d3a
AayushLad <aayush.lad@roimaint.com>
2026-01-20 20:37:30 +0530

    almost completed the product catalog page

---
+++ src/components/add-product-dialog.tsx
@@ -0,0 +1,86 @@
+import { X } from "lucide-react";
+import GhostButton from "./ui/ghost-button";
+import Button from "./ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from "zod";
+import type { ProductType } from "../types/product-type";
+
+type Props = {
+    addProductDialog: boolean,
+    setAddProductDialog: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const AddFormSchema = z.object({
+    name: z.string().min(1, "The Product name is requred."),
+    category: z.string().min(1, "The Product category is requred."),
+    price: z.number().min(1, "Product price can not be zero or less"),
+    stockQuontity: z.number().min(1, "Product quontity can not be zero or less")
+})
+
+function AddProductDialog({ addProductDialog, setAddProductDialog }: Props) {
+    const form = useForm<ProductType>({
+        resolver: zodResolver(AddFormSchema),
+    })
+
+    const handleSubmit = (data: ProductType) => {
+        const prevProducts = JSON.parse(localStorage.getItem("products") ?? "[]") as ProductType[] ?? [];
+        localStorage.setItem("products", JSON.stringify([
+            ...prevProducts,
+            {
+                name: data.name,
+                category: data.category,
+                price: data.price,
+                stockQuontity: data.stockQuontity
+            }
+        ]))
+        form.reset();
+        setAddProductDialog(false);
+    }
+
+    const onInvalid = (errors: typeof form.formState.errors) => {
+        const messages = Object.values(errors).map((err) => err.message);
+        messages.reverse().forEach((msg) => console.log(msg));
+    };
+
+    return (
+        <div className="border border-b-amber-800 fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
+            <div className="w-[400px] h-auto py-4 px-4 bg-cyan-700 rounded-2xl ">
+                <form onSubmit={form.handleSubmit(handleSubmit, onInvalid)} className="flex flex-col gap-5">
+                    <div className="flex justify-between items-center">
+                        <h2 className="text-xl font-bold">Add a product to catalog</h2>
+                        <GhostButton onClick={() => setAddProductDialog(false)}>
+                            <X />
+                        </GhostButton>
+                    </div>
+
+                    <div className="flex flex-col gap-2 px-3">
+                        <div className="flex gap-1.5 justify-between">
+                            <label htmlFor="name">Product Name</label>
+                            <input type="text" id="name" className="border-2 rounded-sm" {...form.register("name")} />
+                        </div>
+                        <div className="flex gap-1.5 justify-between">
+                            <label htmlFor="category">Category</label>
+                            <input type="text" id="category" className="border-2 rounded-sm" {...form.register("category")} />
+                        </div>
+                        <div className="flex gap-1.5 justify-between">
+                            <label htmlFor="price">Price</label>
+                            <input type="number" id="price" defaultValue={0} className="border-2 rounded-sm" {...form.register("price", { valueAsNumber: true })} />
+                        </div>
+                        <div className="flex gap-1.5 justify-between">
+                            <label htmlFor="stock">Stock Quantity</label>
+                            <input type="number" id="stock" defaultValue={1} className="border-2 rounded-sm" {...form.register("stockQuontity", { valueAsNumber: true })} />
+                        </div>
+                    </div>
+
+                    <div className="flex gap-3 justify-end">
+                        <Button onClick={() => setAddProductDialog(false)}>Cancel</Button>
+                        <Button type="submit">Submit</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default AddProductDialog;
--- src/pages/product-catelog-page.tsx
+++ src/pages/product-catelog-page.tsx
@@ -1,5 +1,77 @@
+import { useState } from "react";
+import AddProductDialog from "../components/add-product-dialog";
+import Button from "../components/ui/button";
+import type { ProductType } from "../types/product-type";
+
 function ProductCatelogPage() {
-    return <div>Product catelog page</div>
+    //TODO: proper naiming
+    const [addProductDialog, setAddProductDialog] = useState<boolean>(false);
+    const [searchString, setSearchString] = useState<string>("");
+    const products = JSON.parse(localStorage.getItem("products") ?? "[]") as ProductType[];
+
+    return (
+        <div className="h-[calc(100vh-50px)]">
+            <div className="border-b w-full h-[100px] flex items-center justify-between px-5">
+                <div>
+                    <h1 className="font-bold text-2xl">Product Catalog</h1>
+                    <p className="text-sm">Manage all the products in you catalog from here</p>
+                </div>
+
+                <div>
+                    <Button onClick={() => setAddProductDialog(true)}>
+                        + Add Product
+                    </Button>
+                </div>
+            </div>
+
+            <div className="border border-cyan-400 py-15 px-10 flex flex-col gap-4">
+                <div>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Search with product name"
+                            value={searchString}
+                            onChange={e => setSearchString(e.target.value)}
+                            className="border rounded-lg py-1 px-3 w-[300px]"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-wrap gap-5">
+                    {
+                        products.map(product => {
+                            if (searchString) {
+                                if (!product.name.includes(searchString)) {
+                                    return;
+                                }
+                            }
+
+                            return (
+                                <div className="border rounded-2xl px-2 py-2 w-[200px]">
+                                    <div className="text-lg font-semibold">
+                                        {product.name}
+                                    </div>
+                                    <div>
+                                        Category: {product.category}
+                                    </div>
+                                    <div>
+                                        Price: {product.price}
+                                    </div>
+                                    <div>
+                                        Stock Quontity: {product.price}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+            {addProductDialog &&
+                <AddProductDialog addProductDialog={addProductDialog} setAddProductDialog={setAddProductDialog} />
+            }
+        </div>
+    )
 }
 
 export default ProductCatelogPage;
