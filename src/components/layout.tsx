ea58aa9ed40b5835fcde89f92a0cbbd8815a634e
AayushLad <aayush.lad@roimaint.com>
2026-01-20 18:44:05 +0530

    feat: make navbar functional

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './navbar';
+import { useState } from 'react';
+import Header from './header';
 
 function Layout() {
+    const [isNavOpen, setIsNavOpen] = useState<boolean>(true);
+
     return (
-        <div>
-            <Navbar /> This NavigationBar component is a static part of the layout
-            <div>
-                <Outlet />  {/* This is where child components will be displayed */}
+        <div className=' w-full h-screen flex'>
+            <Navbar isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
+            <div className='flex-1 border border-amber-300'>
+                <Header isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+                <Outlet />
             </div>
         </div>
     );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,40 @@
-function Navbar() {
-    return <div>Navbar</div>
+import { X } from "lucide-react";
+import { Link } from "react-router-dom";
+import GhostButton from "./ui/ghost-button";
+
+type Props = {
+    isOpen: boolean,
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function Navbar({ isOpen, setIsOpen }: Props) {
+    return (
+        <div className={`border border-orange-600 h-screen w-[170px] absolute top-0 ${isOpen ? "left-0" : "-left-[170px]"} lg:w-[250px] lg:static flex flex-col bg-[#242424]`}>
+            <div className="ml-auto lg:hidden">
+                <GhostButton onClick={() => setIsOpen(false)}>
+                    <X className="h-8 w-8" />
+                </GhostButton>
+
+            </div>
+            <nav className="w-full my-10 flex flex-col gap-3 px-3 py-2">
+                <div className="w-full">
+                    <Link to="/dashboard" className="py-1 ">
+                        Dashboard
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/product-catelog" className="py-1 ">
+                        Product Category
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/product-categories" className="py-1 ">
+                        Product Categories
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default Navbar;
